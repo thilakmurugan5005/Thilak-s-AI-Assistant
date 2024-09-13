05a2d3394f98c55dca37a6cef6fe0644d3fd59c2
Thilak Murugan Rajsekar <thilakmurugan5005@gmail.com>
2024-09-13 14:50:19 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
