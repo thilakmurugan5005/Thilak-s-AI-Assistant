58ec8b7f28ae63624607edcd9837b7a8a4d41f50
Thilak Murugan Rajsekar <thilakmurugan5005@gmail.com>
2024-09-13 16:49:36 -0400

    Update Create_DB.py

--- Create_DB.py
+++ Create_DB.py
@@ -5,27 +5,31 @@ from langchain_community.vectorstores import FAISS
 import faiss
 import numpy as np
 import streamlit as st
+import PyPDF2
 
 api_key = st.secrets["OPENAI_API_KEY"]
-def Create_DB():
-    # Step 1 - load and split documents
-    pdf_loader = PyPDFDirectoryLoader("Personal_data.pdf")
-    loaders = [pdf_loader]
 
-    documents = []
-    for loader in loaders:
-        documents.extend(loader.load())
+
+def Create_DB():
+    file_path = 'Personal_data.pdf'
+
+    with open(file_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page in reader.pages:
+            text += page.extract_text()
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=100)
-    all_documents = text_splitter.split_documents(documents)
+    all_text = text_splitter.split_text(text)
 
-    # Step 2 - Create embedding endpoint
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small",openai_api_key=api_key)
+        # Step 2 - Create embedding endpoint
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002",openai_api_key=api_key)
 
-    # create vector db
-    vector_db=FAISS.from_documents(all_documents, embedding=embeddings)
-    # save vector db
-    vector_db.save_local("index_test3")
+        # create vector db
+    vector_db=FAISS.from_texts(all_text, embedding=embeddings)
+        # save vector db
+    vector_db.save_local("index_te123")
 
+ 
 
     return 
